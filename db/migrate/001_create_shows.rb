871072a3fc874baaa013731bae28e5a85469a3c3
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-29 17:22:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,8 +1,11 @@
   class  CreateShows< ActiveRecord::Migration[5.2]
   
   def change
-    create_table  do |t|
-      
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
   end
 end
